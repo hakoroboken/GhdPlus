679eb3df06d2336788329a15117a6e7ecdace337
motii8128 <default270robo@gmail.com>
2026-03-18 11:01:41 +0900

    [update] マイコンとの通信が一応完成

--- Assets/Scripts/MicroControllerConnector.cs
+++ Assets/Scripts/MicroControllerConnector.cs
@@ -15,20 +15,43 @@ public class MicroControllerConnector : MonoBehaviour
     [Tooltip("ROS2UnityComponentをつけたオブジェクトを入れる")]
     public ROS2UnityComponent ROSComponent;
     public string node_name = "MicroControllerConnector";
+    public string send_topic_name = "/to_mc";
+    public string recv_topic_name = "/from_mc";
+    public string status_topic_name = "/status";
 
     [Header("UDP通信設定")]
     public string myIP = "192.168.11.1";
+    public int myPort = 64201;
+    public string destIP = "192.168.11.1";
+    public int destPort = 64202;
     private ROS2Node node;
     private FastUdpServer udp_;
     // Start is called before the first frame update
     void Start()
     {
-        node = ROSComponent.CreateNode(node_name);
-        
+        udp_ = new FastUdpServer(myIP, myPort, destIP, destPort);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(ROSComponent.Ok())
+        {
+            if(node == null)
+            {
+                node = ROSComponent.CreateNode(node_name);
+
+                bool udp_status = udp_.Start(node, send_topic_name, recv_topic_name, status_topic_name);
+
+                if(udp_status)
+                {
+                    Debug.Log($"[{node_name}] 初期化成功");
+                }
+                else
+                {
+                    Debug.LogError($"");
+                }
+            }
+        }
     }
 }
